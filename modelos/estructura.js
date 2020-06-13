c636eaaa91e814edc0b25716f736a5b760be574b
zaholy16 <60717117+zaholy16@users.noreply.github.com>
2020-06-13 17:44:41 -0500

    Estructura
    Estrucutura con solamente la función de agregar.

--- modelos/estructura.js
+++ modelos/estructura.js
@@ -1,23 +1,10 @@
-const objeto = require('./artista');
-
 class Estructura {
-    constructor() {
-        this.miInfo = new Array;
-    }
-
-    //Metodos Clasicos
+  constructor(){
+    this.miInfo = new Array();
+  }
+    
     agregar(nuevo) {
       this.miInfo.push(nuevo);
-    }
-
-    mostrar()
-    {
-      let salida=' ';
-      for(var i=0; i<this.miInfo.length; i++)
-      {
-        salida += "<p>" + this.miInfo.showInfo() + "</p>";
-      }
-      return salida;
     }
 
 }
